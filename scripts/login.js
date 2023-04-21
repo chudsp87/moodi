2e963d67157446ba55e63edf96fc6c2b553e7425
chris <chudsp87@vt.edu>
2023-04-21 13:50:47 -0400

    first commit

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+addEventListener("mouseover", (event) => {});
+
+onmouseover = (event) => {};
+var image = document.getElementsByClassName("image2");
+image.src = "image1.jpg"
+function icon_switch()
+{   
+
+    var edit_save = document.getElementById("edit-save");
+
+       edit_save.src = "../template/save.png";                              
+}
+
+function icon_switch() {
+  document.getElementById("facebook-button-logo").src="images/logo-facebook-blue.svg";
+}
